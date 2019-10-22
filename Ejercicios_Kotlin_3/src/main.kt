c942c233e525ce73616a627538fbf4f57b14f747
David Moreno <dmoreno788@gmail.com>
2019-10-22 17:53:41 +0200

    Actividad 3 completada

---
+++ Ejercicios_Kotlin_3/src/main.kt
@@ -0,0 +1,21 @@
+fun main(){
+
+    println("Introduce cuantos numeros para hacer la media: ")
+    val num = readLine()!!.toInt()
+    println("Introduce los $num para la media: ")
+
+
+    var num2= 0
+    for (i in 1..num) {
+
+        val n = readLine()!!.toInt()
+        num2+=n;
+    }
+
+    println("La media de lo numeros introducidos es :" +num2/num);
+}
+
+fun media(a: Int, b: Int){
+ return
+
+}
