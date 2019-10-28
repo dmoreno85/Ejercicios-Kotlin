0ff32323c5d5b09342fc05367fac6a1955b09603
David Moreno <dmoreno788@gmail.com>
2019-10-28 17:14:00 +0100

    Actividad 10 completada

--- Ejercicios_Kotlin_10/src/main.kt
+++ Ejercicios_Kotlin_10/src/main.kt
@@ -1,6 +1,26 @@
 
-fun main(){
-    
+fun main() {
+
+    println("introduce un numero entre 7 y 30: ")
+    var num = readLine()!!.toInt();
+    var respaldo = num / 2
+    var sillon = respaldo +1
+    var patas = respaldo -1
+
+    for (i in 1..respaldo) {
+
+        println("*")
+    }
+    for (j in 1..sillon) {
+        print("*")
+    if (j == sillon) println("")
+    }
+    for (h in 1..patas) {
+
+        for (w in 1..sillon){
+            if ( w == 1 || w == sillon)print("*")
+            else print(" ")
+        }
+        println()
+    }
 }
-
-
